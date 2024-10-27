ae628b48c6a4c90b94df2076859ae3f22b1c53c5
adriano-gil <adrianoagiljr@gmail.com>
2024-10-27 16:06:20 -0300

    att detona ralph

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -11,7 +11,7 @@ const state = {
         gameVelocity: 600,
         hitPosition: 0,
         result: 0,
-        currentTime: 61,
+        currentTime: 60,
     },
 
     actions: {
@@ -20,21 +20,30 @@ const state = {
     },
 };
 
+function playSound(audioName){
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.12;
+    audio.play();
+}
+
 function countDown() {
     state.values.currentTime --;
     state.view.timeLeft.textContent = state.values.currentTime;
 
     if(state.values.currentTime === 0) {
+
+        if(state.values.result === 0) {
+            clearInterval(state.actions.countDownTimerId)
+            clearInterval(state.actions.timerId)
+            clearInterval(state.values.hitPosition = null)
+            playSound("fail");
+        } else {
         clearInterval(state.actions.countDownTimerId)
         clearInterval(state.actions.timerId)
-        alert("Game Over! O seu Resultado foi: " + state.values.result + " pontos!")
+        clearInterval(state.values.hitPosition = null)
+        playSound("gameover");
+        }        
     }
-}
-
-function playSound(audioName){
-    let audio = new Audio(`./src/audios/${audioName}.m4a`);
-    audio.volume = 0.12;
-    audio.play();
 }
 
 function randomSquare() {
@@ -54,14 +63,16 @@ function addListenerHitbox(){
             if(square.id === state.values.hitPosition){
                 state.values.result++;
                 state.view.score.textContent = state.values.result;
-                state.values.hitPosition = null;
                 playSound("hit");
+            } else {
+                playSound("wronghit");
             }
         });
     });
 }
 
 function initialize(){
+    alert("Bem vindo! aperte (ok) para começar!")
     addListenerHitbox();
 }
 
