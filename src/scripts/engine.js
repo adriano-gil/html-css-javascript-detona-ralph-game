0569e071e77474adcc9398bc3f34ae3a972da4f2
adriano-gil <adrianoagiljr@gmail.com>
2024-10-27 16:25:12 -0300

    att detona ralph

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -63,6 +63,7 @@ function addListenerHitbox(){
             if(square.id === state.values.hitPosition){
                 state.values.result++;
                 state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null
                 playSound("hit");
             } else {
                 playSound("wronghit");
